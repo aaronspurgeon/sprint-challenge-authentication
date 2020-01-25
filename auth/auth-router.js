c10a5e0b7f61c3f71c1c4af401cd3e60781020b5
Aaron Spurgeon <aspurgeon061@gmail.com>
2020-01-24 19:18:48 -0500

    Added the functionality for registering and loggin in

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -4,11 +4,37 @@ const usersModel = require("../users/users-model");
 const restricted = require("./authenticate-middleware");
 
 router.post("/register", (req, res) => {
-  // implement registration
+  try {
+    const saved = await usersModel.add(req.body);
+
+    res.status(201).json(saved);
+  } catch (err) {
+    next(err);
+  }
 });
 
 router.post("/login", (req, res) => {
-  // implement login
+  try {
+    const { username, password } = req.body;
+    const user = await usersModel.findBy({ username }).first();
+    // since bcrypt hashes generate different results due to the salting,
+    // we rely on the magic internals to compare hashes (rather than doing
+    // it manulally by re-hashing and comparing)
+    const passwordValid = await bcrypt.compare(password, user.password);
+
+    if (user && passwordValid) {
+      req.session.user = user;
+      res.status(200).json({
+        message: `Welcome ${user.username}!`
+      });
+    } else {
+      res.status(401).json({
+        message: "Invalid Credentials"
+      });
+    }
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
